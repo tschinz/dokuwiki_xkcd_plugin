8f5d518a1b952a3bafa13bd88c45da89d23c7a6b
tschinz <zaswiki@gmail.com>
2012-10-22 15:20:46 +0200

    v1.0 github repo creation

---
+++ README.md
@@ -0,0 +1,65 @@
+# Dokuwiki Plugin XKCD
+
+<table>
+  <tr>
+    <th align="left">Description</th>
+    <td>The Plugin display the lastest xkcd comic</td>
+  </tr>
+  <tr>
+    <th align="left">Author</th>
+    <td>Zahno Silvan</td>
+  </tr>
+  <tr>
+    <th align="left">Email</th>
+    <td>zaswiki@gmail.com</td>
+  </tr>
+  <tr>
+    <th align="left">Type</th>
+    <td>syntax</td>
+  </tr>
+  <tr>
+    <th align="left">Lastupdate</th>
+    <td>2012-10-22</td>
+  </tr>
+  <tr>
+    <th align="left">Tags</th>
+    <td>comic, rss, xkcd, strip</td>
+  </tr>
+</table>
+
+## Download
+* Download to Dokuwiki plugin folder
+* File     : https://github.com/tschinz/dokuwiki_xkcd_plugin/zipball/master
+
+## Versions
+* **2012-01-06**
+  * Initial release
+* **2012-09-25**
+  * Patch from kenneth
+* **2012-10-22**
+  * Moved repo to github
+
+## Syntax
+The Comic is displayed with the following syntax:
+```
+[xkcd]
+```
+## Want the old plugin?
+Download here the code from the old plugin created by Jeff Sacco It doesn't work on Rincewind and Angua.
+
+[Old xkcd Plugin](http://zawiki.dyndns.org/~zas/zawiki/schinz:programming:dw:xkcd:plugin-xkcd_old_jeff_sacco.tgz)
+
+## Thanks
+* Xkcd for amazing comics
+* Kenneth for the patch
+* Jeff Sacco for the old plugin
+
+## Documentation
+
+All documentation for the XKCD Plugin is available online at:
+
+  * [Dokuwiki Plugin Page](http://dokuwiki.org/plugin:xkcd)
+  * [ZaWiki Plugin Page](http://zawiki.dyndns.org/~zas/zawiki/doku.php/tschinz:dw_xkcd)
+  * [Github Project Page](https://github.com/tschinz/dokuwiki_xkcd_plugin)
+
+(c) 2011 by Zahno Silvan <zaswiki@gmail.com>
---
+++ syntax.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Plugin xkcd: display xkcs comic
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Carlo Perassi <carlo@perassi.org>
+ * @author     Zahno Silvan <zaswiki@gmail.com>
+ */
+
+// based on http://wiki.splitbrain.org/plugin:tutorial
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) die();
+
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+require_once(DOKU_PLUGIN . 'syntax.php');
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_xkcd extends DokuWiki_Syntax_Plugin {
+    function getInfo() {
+        return array(
+        'author'  => 'Zahno Silvan, Carlo Perassi',
+        'email'   => 'zaswiki@gmail.com',
+        'date'    => '2012-09-25',
+        'name'    => 'xkcd Plugin',
+        'desc'    => 'It displays the xkcd treee times a week. Using RSS feed',
+        'url'     => 'http://xkcd.com/rss.xml'
+        );
+    }
+
+    private function _listhd() {
+        require_once(DOKU_INC . 'inc/HTTPClient.php');
+
+        $url = 'http://xkcd.com/rss.xml';
+        $ch = new DokuHTTPClient();
+        $piece = $ch->get($url);
+        $xml = simplexml_load_string($piece);
+        
+        $comicURL = $xml->channel->item->link;
+
+        $description = (string) $xml->channel->item->description;
+        $description = html_entity_decode($description);
+        $dom = new DOMDocument();
+        $dom->loadXML($description);
+        $imgSrc = $dom->childNodes->item(0)->attributes->getNamedItem('src' )->value;
+        $imgTitle = $dom->childNodes->item(0)->attributes->getNamedItem('title' )->value;
+ 
+        $feed_contents = "<a href=\"$comicURL\"><img src=\"$imgSrc\" title=\"$imgTitle\" /></a>\n";
+  
+        return $feed_contents;
+    }
+
+
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('\[xkcd\]', $mode, 'plugin_xkcd');
+    }
+
+    function getType() { return 'substition'; }
+
+    function getSort() { return 667; }
+
+    function handle($match, $state, $pos, &$handler) {
+        return array($match, $state, $pos);
+    }
+
+    function render($mode, &$renderer, $data) {
+
+        if ($mode == 'xhtml') {
+            $renderer->doc .= $this->_listhd();
+            return true;
+        }
+        return false;
+    }
+}
